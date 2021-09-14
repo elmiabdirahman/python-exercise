b82b26e9b6d64cf0c12c5a71671a045a7b78150a
Elmi Elmi <elmielmi522@gmail.com>
2021-09-13 22:06:48 -0500

    added usefull string methods

--- formatted-strings.py
+++ formatted-strings.py
@@ -2,3 +2,15 @@ first = "Tom"
 last = "Ali"
 full = f"{len(first)} {2 + 2}"
 print(full)
+
+## usefull string methods
+course = "  Python Programming"
+print(course.upper())
+print(course.lower())
+print(course.title())
+
+print(course.strip())
+
+print(course.find("Pro"))
+print(course.replace("P", "__"))
+print("Programming" not in course)
