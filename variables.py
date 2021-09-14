6799f0f5cd119cb384af4ad845c212063661da81
Elmi Elmi <elmielmi522@gmail.com>
2021-09-13 20:45:33 -0500

    variables

---
+++ variables.py
@@ -0,0 +1,12 @@
+students_count = 1000
+rating = 4.99
+is_published = False
+course_name = "Strings"
+
+# initialize into one line
+x = 1
+y = 2
+x, y = 1, 2  # short cut
+
+# multiple variables to the same value
+x = y = 1
