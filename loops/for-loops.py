56a5b76a0300ed310f9b3c5b3436e62d51b4b7f8
Elmi Elmi <elmielmi522@gmail.com>
2021-09-18 20:04:30 -0500

    for loops

---
+++ loops/for-loops.py
@@ -0,0 +1,13 @@
+#for x in "python":
+#   print(x)
+
+##print out a list
+#for x in ['a', 'b', 'c']:
+#    print(x)
+
+##prints out the range of numbers
+#for x in range(0, 10, 2):
+#    print(x)
+
+print(type(range(5)))
+print([1, 2, 3, 4, 5])
