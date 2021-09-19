1fcdd90dc374da201487a4a7f4609072fe6af1c0
Elmi Elmi <elmielmi522@gmail.com>
2021-09-18 21:19:24 -0500

    type of functions

---
+++ functions/type-of-functions.py
@@ -0,0 +1,12 @@
+def greet(name):
+    print(f"Hi {name}")
+
+print(greet("Elmi"))
+
+#def get_greeting(name):
+ #   return f"Hi {name}"
+
+
+#message = get_greeting("Elmi")
+#file = open("content.text", "w")
+#file.write(message)
